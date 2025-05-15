0ce5bb3f59d6ba4162b7c831e9a91611ca918763
jasokwin <jasokwin@gmail.com>
2025-05-15 13:15:54 +0700

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -5,16 +5,22 @@ export default async function handler(req, res) {
   const from = today.toISOString().split('T')[0];
 
   const toDate = new Date();
-  toDate.setDate(today.getDate() + 3); // 3 ngày tới
+  toDate.setDate(today.getDate() + 3);
   const to = toDate.toISOString().split('T')[0];
 
-  const response = await fetch(`https://v3.football.api-sports.io/fixtures?from=${from}&to=${to}`, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "v3.football.api-sports.io",
-      "x-rapidapi-key": apiKey
+  const leagueId = 39; // Premier League
+  const season = 2024;
+
+  const response = await fetch(
+    `https://v3.football.api-sports.io/fixtures?from=${from}&to=${to}&league=${leagueId}&season=${season}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "v3.football.api-sports.io",
+        "x-rapidapi-key": apiKey
+      }
     }
-  });
+  );
 
   const data = await response.json();
   res.status(200).json(data);
